41b11311e7f09b34fa1a8f7bbae56936917dd8b3
saynschuman <saynschuman@gmail.com>
2017-03-17 18:18:00 +0200

    desktop finished

--- dist/js/main.js
+++ dist/js/main.js
@@ -397,6 +397,15 @@ $('#lightSlider_2').lightSlider({
   // $('.n-1').animateNumber({ number: 1000 });
 
 
-  // form-2
+  // map
+
+    var myMap;
+  ymaps.ready(init); // Ожидание загрузки API с сервера Яндекса
+  function init () {
+    myMap = new ymaps.Map("map", {
+      center: [55.76, 37.64], // Координаты центра карты
+      zoom: 10 // Zoom
+    });
+  }
 
 }(jQuery));
