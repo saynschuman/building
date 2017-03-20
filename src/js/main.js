a3242ba19db74513e6de200a8a833a0e80cb8854
saynschuman <saynschuman@gmail.com>
2017-03-20 13:12:04 +0200

    desktop finished

--- src/js/main.js
+++ src/js/main.js
@@ -363,7 +363,27 @@
 
   // lightSlider
 
-$('#lightSlider').lightSlider({
+ $('#slick-slider-outer').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  arrows: false,
+  asNavFor: '#slick-slider-inner'
+});
+$('#slick-slider-inner').slick({
+  slidesToShow: 7,
+  slidesToScroll: 1,
+  asNavFor: '#slick-slider-outer',
+  focusOnSelect: true
+});
+  
+$('.wr_2').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1
+});
+
+
+
+$('#lightSlider_2').lightSlider({
     gallery: true,
     item: 1,
     loop: true,
@@ -399,13 +419,62 @@ $('#lightSlider_2').lightSlider({
 
   // map
 
-    var myMap;
-  ymaps.ready(init); // Ожидание загрузки API с сервера Яндекса
-  function init () {
-    myMap = new ymaps.Map("map", {
-      center: [55.76, 37.64], // Координаты центра карты
-      zoom: 10 // Zoom
-    });
-  }
+    ymaps.ready(init); // карта соберется после загрузки скрипта и элементов
+        var myMap; // заглобалим переменную карты чтобы можно было ею вертеть из любого места
+        function init () { // функция - собиралка карты и фигни
+            myMap = new ymaps.Map("map", { // создаем и присваиваем глобальной переменной карту и суем её в див с id="map"
+                    center: [60.00808710199225,32.15604568721752], // ну тут центр 
+                    behaviors: ['default', 'scrollZoom'], // скроллинг колесом
+                    zoom: 16 // тут масштаб
+                });
+            // myMap.controls // добавим всяких кнопок, в скобках их позиции в блоке
+            // .add('zoomControl', { left: 5, top: 5 }) //Масштаб
+            // .add('typeSelector') //Список типов карты
+            // .add('mapTools', { left: 35, top: 5 }) // Стандартный набор кнопок
+            // .add('searchControl'); // Строка с поиском
+          /* Создаем кастомные метки */
+          myPlacemark0 = new ymaps.Placemark([60.00808710199225,32.15604568721752], { // Создаем метку с такими координатами и суем в переменную
+                  balloonContent: '<div class="ballon"><img src="img/hh.jpg" class="ll"/><span>Заголовок метки 1</span><br/><p>Немного инфы о том, о сем. Лорем ипсум чото там.</p><img class="close" onclick="myMap.balloon.close()" src="img/close.png"/></div>' // сдесь содержимое балуна в формате html, все стили в css
+                }, {
+                iconImageHref: 'img/map-tag.png', // картинка иконки
+                iconImageSize: [36, 56], // размер иконки
+                iconImageOffset: [-232, -64], // позиция иконки
+                  balloonContentSize: [270, 99], // размер нашего кастомного балуна в пикселях
+                  balloonLayout: "default#imageWithContent", // указываем что содержимое балуна кастомная херь
+                  balloonImageHref: 'img/map-tag.png', // Картинка заднего фона балуна
+                  balloonImageOffset: [-65, -89], // смещание балуна, надо подогнать под стрелочку
+                  balloonImageSize: [260, 89], // размер картинки-бэкграунда балуна
+                  balloonShadow: false,
+                  balloonAutoPan: false // для фикса кривого выравнивания
+                  });
+          /* Добавляем */
+          myMap.geoObjects
+            .add(myPlacemark0)
+
+          /* Фикс кривого выравнивания кастомных балунов */
+      myMap.geoObjects.events.add([
+            'balloonopen'
+        ], function (e) {
+            var geoObject = e.get('target');
+            myMap.panTo(geoObject.geometry.getCoordinates(), {
+                                        delay: 0
+                                    });
+        });
+    }
+
+
+
+
+    $('#next-step').click(function(){
+      var slider = $("#lightSlider_2").lightSlider();  
+      slider.goToSlide(3);
+    })
+    
+    $('.carousel').carousel();
+
+    $(window).on('load resize', function(){
+      var c_b_r = $('.container').offset().left;
+    $('.contacts-block').css('right', c_b_r);
+    })
 
 }(jQuery));
